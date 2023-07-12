6e59875e3d918b5998bef9008b89b2a32ff341f8
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-07-12 17:15:53 +0200

    updated index.html
    added some styles using tailwind and on css classic

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const mainContainer = document.querySelector(".container")
-const reviewContainer = document.querySelector(".review")
-const submitButton = document.getElementById("submit-rating")
-const rateAgain = document.getElementById("rate-again")
-const ratings = document.querySelectorAll(".btn")
-const reviewRating = document.getElementById("rating")
+const mainContainer = document.querySelector(".container");
+const reviewContainer = document.querySelector(".review");
+const submitButton = document.getElementById("submit-rating");
+const rateAgain = document.getElementById("rate-again");
+const ratings = document.querySelectorAll(".btn");
+const reviewRating = document.getElementById("rating");
 
- submitButton.addEventListener("click", () => {
-    mainContainer.style.display = "none"
-    reviewContainer.classList.remove("hidden")
+submitButton.addEventListener("click", () => {
+    mainContainer.style.display = "none";
+    reviewContainer.classList.remove("hidden");
+});
 
- ratings.forEach(rating) => {
+ratings.forEach((rating) => {
     rating.addEventListener("click", () => {
-        reviewRating.innerHTML = rating.innerHTML
-    })
- })
+        reviewRating.innerHTML = rating.innerHTML;
+    });
+});
 
- 
- rateAgain.addEventListener("click", () => {
-    mainContainer.style.display = "block"
-    reviewContainer.classList.add("hidden")
+rateAgain.addEventListener("click", () => {
+    mainContainer.style.display = "block";
+    reviewContainer.classList.add("hidden");
+});
 
- })
