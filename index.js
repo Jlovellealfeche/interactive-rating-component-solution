d1cf1e73299263b38947107a3d6db48aa169842d
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-07-12 15:12:43 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-function rate(rating) {
-    selectedRating = rating
-
-    const numberRate = document.getElementsByClassName('rate');
-    for (let i=0; i< rate.length; i++) {
-        if (i < rating) {
-            rate[i].classList.add('selected');
-        } else {
-            rate[i].classList.remove('selected')
-        }
-
-        }
-    }
-
-
-
-
-    function submitRating() {
-        const reviewMessage = document.getElementById('reviewMessage').value;
-      
-        if (selectedRating === 0) {
-          alert('Please select a rating before submitting.');
-          return;
-        }
-      
-        // Display the selected rating and review message
-        document.getElementById('result').textContent = `You rated: ${selectedRating} rate\nReview: ${reviewMessage}`;
-      }
+const mainContainer = document.querySelector(".container")
+const reviewContainer = document.querySelector(".review")
+const submitButton = document.getElementById("submit-rating")
+const rateAgain = document.getElementById("rate-again")
+const ratings = document.querySelectorAll(".btn")
+const 
